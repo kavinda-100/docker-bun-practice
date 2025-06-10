777e8a9e417f3308015f1616db53d9f6c1b32e7a
kavinda-100 <kavindarathnayake100@gmail.com>
2025-06-10 11:26:47 +0530

    feat: add uuid dependency and update post route response to include generated UUID

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -1,10 +1,11 @@
 import express, {type Request, type Response} from "express";
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
 // post route
 router.get("/", (req: Request, res: Response) => {
-    res.send("Post route is working! 🚀");
+    res.send(`Post route is working! 🚀. Post id is - ${uuidv4()}`);
 })
 
 export default router;
